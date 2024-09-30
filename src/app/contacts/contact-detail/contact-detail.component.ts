b361d4343af690832902b855afea9cad2c398b0d
mmullerx4 <mmullerx4@gmail.com>
2024-09-30 13:38:56 -0400

    details

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model'
 
 @Component({
   selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrl: './contact-detail.component.css'
+  styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent {
+  SelectedContact: Contact;
+  contacts: Contact[];
 
+  constructor() {
+    this.SelectedContact = {
+      id: '',
+      name: '',
+      email: '',
+      phone: '',
+      imageUrl: '',
+      group: []
+
+    }
+  }
 }
