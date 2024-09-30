4201b4c9205c86186ce0fe4717f6c3c7bf2663df
mmullerx4 <mmullerx4@gmail.com>
2024-09-30 11:08:03 -0400

    remove header from sub app/app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { HeaderComponent } from './app/header.component';
+import { HeaderComponent } from './header.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 
