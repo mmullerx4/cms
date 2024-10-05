80f8447d5de665c220e5ce9446b744e6286966bd
mmullerx4 <mmullerx4@gmail.com>
2024-10-05 12:46:19 -0400

    w03-add contact-item

---
+++ src/app/contacts/contact-item/contact-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-contact-item',
+  templateUrl: './contact-item.component.html',
+  styleUrl: './contact-item.component.css'
+})
+export class ContactItemComponent {
+
+}
