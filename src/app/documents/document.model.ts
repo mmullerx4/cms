091ea2dba605d6ef0d50e73109cd535b921ee25f
mmullerx4 <mmullerx4@gmail.com>
2024-10-06 20:08:29 -0400

    w03-messages setup

--- src/app/documents/document.model.ts
+++ src/app/documents/document.model.ts
@@ -1,2 +1,15 @@
 export class Document {
+  public id: string;  //choosing to store id & phone as string
+  public name: string;
+  public description: string;
+  public imageUrl: string;
+  public children: Document[];
+
+  constructor(id: string, name: string, description: string,  imageUrl: string, children: Document[] = []) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.children = children;
+  }
 }
