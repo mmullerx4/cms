1e04a98dde9e9632f5827ef2fdae00b198d63674
mmullerx4 <mmullerx4@gmail.com>
2024-10-07 12:19:43 -0400

    rollback & edit files

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -4,12 +4,28 @@ import { Message } from '../message.model';
 @Component({
   selector: 'cms-message-edit',
   templateUrl: './message-edit.component.html',
-  styleUrl: './message-edit.component.css'
+  styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent {
-  @ViewChild('subject') subjectInputRef: ElementRef;
-  @ViewChild('msgText') msgTextInputRef: ElementRef;
+  @ViewChild('subjectInput') subjectInputRef: ElementRef;
+  @ViewChild('msgTextInput') msgTextInputRef: ElementRef;
 
   @Output() addMessageEvent = new EventEmitter<Message>();
 
+  currentSender = 'John Doe';
+
+  onSendMessage() {
+    const subjectValue = this.subjectInputRef.nativeElement.value;
+    const msgTextValue = this.msgTextInputRef.nativeElement.value;
+
+    const newMessage = new Message('1', subjectValue, msgTextValue, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = '';
+    this.subjectInputRef.nativeElement.value = '';
+
+  }
 }
