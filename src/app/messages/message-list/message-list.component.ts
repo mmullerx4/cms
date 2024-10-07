539bdf35f79157bf32b06accbfb8ad64545cc9a7
mmullerx4 <mmullerx4@gmail.com>
2024-10-07 19:44:40 -0400

    w03-Display and add message-list

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
   templateUrl: './message-list.component.html',
-  styleUrl: './message-list.component.css'
+  styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent {
+export class MessageListComponent implements OnInit {
+  selectedMessage: Message;
+
+  //initialize with 3 message objects
+  messages: Message[] = [
+    new Message('1', 'Hello World', 'This is the first message', 'Joe'),
+    new Message('2', 'Second Hello!!', 'This is the second message', 'Sage'),
+    new Message('3', 'Third Hello!!', 'This is the third message', 'Tate'),
+  ];
+
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  //to push the new message
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 
 }
