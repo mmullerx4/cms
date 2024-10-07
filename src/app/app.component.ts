5beb38e95c80b004eae3bb3a221ec11d3130a1a2
mmullerx4 <mmullerx4@gmail.com>
2024-10-06 22:19:22 -0400

    w03-msg Edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
-
+  
   selectedFeature: string = 'documents';
 
   switchView(selectedFeature: string) {
--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  @ViewChild('subject') subjectInputRef: ElementRef;
+  @ViewChild('msgText') msgTextInputRef: ElementRef;
+
+  @Output() addMessageEvent = new EventEmitter<Message>();
 
 }
