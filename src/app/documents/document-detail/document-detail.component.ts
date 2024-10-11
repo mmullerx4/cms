68d9eded844644691caa022e354873094e8a9b3d
mmullerx4 <mmullerx4@gmail.com>
2024-10-11 11:07:10 -0400

    w04-ImplementDocDetail

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Document } from  '../document.model';
 
 @Component({
   selector: 'cms-document-detail',
   templateUrl: './document-detail.component.html',
-  styleUrl: './document-detail.component.css'
+  styleUrls: ['./document-detail.component.css']
 })
-export class DocumentDetailComponent {
+export class DocumentDetailComponent implements OnInit {
+  @Input() document: Document;
+
+  constructor() {}
+
+  ngOnInit() {}
 
 }
