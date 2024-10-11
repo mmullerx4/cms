3980b4a7e31bd837b2821d0b0ad180eb9435a023
mmullerx4 <mmullerx4@gmail.com>
2024-10-10 20:02:34 -0400

    ImplementDocList.steps

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
+  styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('1', 'Name1', 'This is the first document', 'Joe'),
+    new Document('2', 'Name2', 'This is the second document', 'Sage'),
+    new Document('3', 'Name3', 'This is the third document', 'Tate'),
+    new Document('4', 'Name4', 'This is the fourth document', 'Ephraim'),
+    new Document('5', 'Name5', 'This is the fifth document', 'Liam')
+  ];
+
+  constructor() {}
+
+  //Lifecyle hook
+  ngOnInit() {
+
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
