817645c41644c550590b01e95a5bdc7519a1b7a1
mmullerx4 <mmullerx4@gmail.com>
2024-10-12 10:58:18 -0400

    w04-ImplementDocComp

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,10 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Document } from './document.model';
 
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
-  styleUrl: './documents.component.css'
+  styleUrls: ['./documents.component.css']
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
+  selectedDocument: Document;
+
+  //documents = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
 
 }
