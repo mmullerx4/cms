399cd0bd5604fa812900837ff8d92d8f668b6d68
mmullerx4 <mmullerx4@gmail.com>
2024-10-18 11:46:07 -0400

    w05-create contact service

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Contact } from "../contacts/contact.model";
+import { MOCKCONTACTS } from './MOCKCONTACTS';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,24 @@ import { Contact } from "../contacts/contact.model";
 export class ContactService {
   private contacts: Contact[] = [];
 
-  constructor() { }
+  // assign the value of the MOCKCONTACTS variable defined in the MOCKCONTACTS.ts file to the contacts class variable in the ContactService class.
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+ }
+
+ getContacts() {
+  return this.contacts.slice(); //return just a copy of the contacts with the slice() so not access original
+ }
+
+//search contacts, return if equal to id or return null
+  getContact(id: string): Contact {
+    for (let contact of this.contacts) {
+      if (contact.id === id) {
+        return contact;
+      }
+    }
+    return null;
+  }
+
+
 }
