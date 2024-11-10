fe537e5bbfca1e4c4a5aa9005a49472ef57d651c
mmullerx4 <mmullerx4@gmail.com>
2024-11-10 15:32:29 -0500

    w08 contact

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,26 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrl: './contact-edit.component.css'
+  styleUrls: ['./contact-edit.component.css']
 })
-export class ContactEditComponent {
-  name: string = '';
-  phone: string = '';
-  email: string = '';
+export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
-  onAddContact() {
-    const newContact = {
-      name: this.name,
-      phone: this.phone,
-      email: this.email
-    };
-    console.log('Contact Added:', newContact);
-  }
+   constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute) {}
 
+    ngOnInit() {
+      this.route.params.subscribe((params: Params) => {
+        const id = params['id'];
 
+        if ( id === undefined || id === null) {
+          this.editMode = false;
+          return;
+        }
+        this.originalContact = this.contactService.getContact(id);
 
+        if (this.originalContact === undefined || this.originalContact === null) {
+          return;
+        }
+        this.editMode = true;
+        this.contact = {...this.originalContact};
+
+        if (this.contact.group) {
+          this.groupContacts = [...this.contact.group];
+        }
+      });
+    }
+
+    onSubmit(form: NgForm) {
+      if (form.valid) {
+        console.log('Form Submitted', this.contact);
+      }
+    }
+
+    onCancel() {
+      console.log('Cancel button clicked');
+      this.router.navigate(['/contacts']);
+    }
 
 
 }
